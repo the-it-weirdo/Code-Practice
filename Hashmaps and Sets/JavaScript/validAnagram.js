7f7df7864629cc8569cbcbca1aa80e5f97c18f22
the-it-weirdo <ddsmegh4@gmail.com>
2021-05-18 21:54:19 +0530

    Optimised validAnagrams in JavaScript

--- Hashmaps and Sets/JavaScript/validAnagram.js
+++ Hashmaps and Sets/JavaScript/validAnagram.js
@@ -8,36 +8,38 @@
 // s = "program", t = "function", return false
 
 const validAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+
   let charMapS = {};
   s.split("").forEach((element) => {
-    charMapS[element] = charMapS[element] ? charMapS[element] + 1 : 1;
+    charMapS[element] = (charMapS[element] || 0) + 1;
   });
-  let charMapT = {};
-  t.split("").forEach((element) => {
-    charMapT[element] = charMapT[element] ? charMapT[element] + 1 : 1;
-  });
-  return Object.entries(charMapS).sort().toString() === Object.entries(charMapT).sort().toString();
+
+  for (let element of t) {
+    if (!charMapS[element]) return false;
+    charMapS[element]--;
+  }
+
+  return true;
 };
 
-
 const test = () => {
-    testCases = [
-      { s: "cat", t: "tac" },
-      { s: "silent", t: "listen" },
-      { s: "program", t: "function" },
-    ];
-    expecteds = [true, true, false];
-  
-    for (let i = 0; i < testCases.length; i++) {
-      const actual = validAnagram(testCases[i].s, testCases[i].t);
-      console.log(`For '${testCases[i]}' output obtained '${actual}'`);
-      console.assert(
-        actual == expecteds[i],
-        `Wrong answer. Expected: '${expecteds[i]}'. Found: '${actual}'`
-      );
-    }
-    console.log("Test completed.");
-  };
-  
-  test();
-  
+  testCases = [
+    { s: "cat", t: "tac" },
+    { s: "silent", t: "listen" },
+    { s: "program", t: "function" },
+  ];
+  expecteds = [true, true, false];
+
+  for (let i = 0; i < testCases.length; i++) {
+    const actual = validAnagram(testCases[i].s, testCases[i].t);
+    console.log(`For '${testCases[i]}' output obtained '${actual}'`);
+    console.assert(
+      actual == expecteds[i],
+      `Wrong answer. Expected: '${expecteds[i]}'. Found: '${actual}'`
+    );
+  }
+  console.log("Test completed.");
+};
+
+test();
